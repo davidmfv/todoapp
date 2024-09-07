93e7a26205a77124ed5b004f1f417f4fc3f9667f
hungndl <hungndl86@gmail.com>
2024-09-07 16:46:26 +0700

    update graphql

--- backend/todo/graphql/src/main/kotlin/dev/hungndl/todo/TodoGraphQLApplication.kt
+++ backend/todo/graphql/src/main/kotlin/dev/hungndl/todo/TodoGraphQLApplication.kt
@@ -3,7 +3,7 @@ package dev.hungndl.todo
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = ["dev.hungndl.todo"])
 class TodoGraphQLApplication
 
 fun main(args: Array<String>) {
