c28a6cd59d52fd3517dcbf05b9165ddee94ca606
hungndl <hungndl86@gmail.com>
2024-09-07 16:56:49 +0700

    update graphql

--- backend/todo/graphql/src/main/kotlin/dev/hungndl/todo/configuration/SecurityConfig.kt
+++ backend/todo/graphql/src/main/kotlin/dev/hungndl/todo/configuration/SecurityConfig.kt
@@ -4,7 +4,11 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
@@ -12,12 +16,25 @@ class SecurityConfig {
 
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        http
-            .cors().and()
-            .csrf().disable()
-            .authorizeHttpRequests { authorize ->
-                authorize.anyRequest().permitAll()
+        http {
+            cors { }
+            csrf { disable() }
+            authorizeRequests {
+                authorize(anyRequest, permitAll)
             }
+        }
         return http.build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("http://localhost:3000")
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        configuration.allowedHeaders = listOf("*")
+        configuration.allowCredentials = true
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/graphql", configuration)
+        return source
     }
 }
